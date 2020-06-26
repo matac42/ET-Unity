b9a8998995495322c588591113bab34658c8f19f
matayu <e185742@ie.u-ryukyu.ac.jp>
2020-06-26 20:51:24 +0900

    create Logo scene.

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Fade : MonoBehaviour
+{
+    float fadeSpeed = 0.02f;
+    float red, green, blue, alfa;
+
+    public bool isFadeIn = false;
+    public bool isFadeOut = false;
+
+    Image fadeImage;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        fadeImage = GetComponent<Image>();
+        red = fadeImage.color.r;
+        green = fadeImage.color.g;
+        blue = fadeImage.color.b;
+        alfa = fadeImage.color.a;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(isFadeIn)
+        {
+            StartFadeIn();
+        }
+        if(isFadeOut)
+        {
+            StartFadeOut();
+        }
+    }
+
+    void StartFadeOut()
+    {
+        fadeImage.enabled = true;
+        alfa += fadeSpeed;
+        SetAlfa();
+        if(alfa >= 1)
+        {
+            isFadeOut = false;
+        }
+    }
+
+    void StartFadeIn()
+    {
+        alfa -= fadeSpeed;
+        SetAlfa();
+        if(alfa <= 0)
+        {
+            isFadeIn = false;
+            fadeImage.enabled = false;
+        }
+    }
+
+    void SetAlfa()
+    {
+        fadeImage.color = new Color(red, green, blue, alfa);
+    }
+}
---
+++ Assets/Scripts/FadeLogo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+public class FadeLogo : Fade
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.Start();
+        base.isFadeOut = true;
+    }
+
+    // Update is called once per frame
+    bool end = false;
+    async void Update()
+    {
+        base.Update();
+
+        if(!isFadeOut && !isFadeIn)
+        {
+            if(end)
+            {
+                SceneManager.LoadScene("Opening");
+            }
+            await Task.Delay(3000);
+            isFadeIn = true;
+            end = true;
+        }
+    }
+}
