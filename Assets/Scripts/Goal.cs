a28c0596393ad23ac04090598789385293c01e55
takarahayato <e185734@ie.u-ryukyu.ac.jp>
2020-07-30 20:23:16 +0900

    add clear message and music

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -5,29 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject GoalText;
+    
     void OnTriggerExit(Collider other)
     {
         
         
         if(other.gameObject.CompareTag("Goal")){
-            Invoke("finish", 1f);
+            GoalText.SetActive(true);
+            
+            
+            Invoke("finish", 3.0f);
             Debug.Log("goal");
+
           }
         
     }
 
     void finish(){
         SceneManager.LoadScene("Ending");
+        
+        
     }
 // Start is called before the first frame update
     void Start()
     {
+        GoalText.SetActive(false);
+        
     
     }
 
 // Update is called once per frame
     void Update()
     {
+    
     
     }
 }
