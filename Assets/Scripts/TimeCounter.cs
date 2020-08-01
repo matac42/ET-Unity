4c619f05d61f0450f225b8b6d78c5f292bcfa12d
matayu <e185742@ie.u-ryukyu.ac.jp>
2020-08-01 19:20:58 +0900

    fix counter

--- Assets/Scripts/TimeCounter.cs
+++ Assets/Scripts/TimeCounter.cs
@@ -6,35 +6,32 @@ using UnityEngine.UI;
 public class TimeCounter : MonoBehaviour
 {
     //カウントアップ
-    private float countup = 0.0f;
+    private float second = 0.0f;
+    private float minute = 0.0f;
+    private float oldSeconds = 0.0f;
 
     //時間を表示するText型の変数
     public Text timeText;
-    
-    //クリックされたかどうか
-    private bool isClick = false;
 
     // Update is called once per frame
     void Update()
     {
-        //クリックされたかどうか
-        if(isClick)
-        {
-            //クリックされた後は以降の処理を実行しないためにreturn
-            return;
-        }
-        
         //時間をカウントする
-        countup += Time.deltaTime;
+        second += Time.deltaTime;
+        Debug.Log(second);
+        if(second >= 60f)
+        {
+            minute ++;
+            second = second - 60;
+        }
 
         //時間を表示する
-        timeText.text = countup.ToString("f1");
+        // timeText.text = second.ToString("f2");
 
-        //クリックした瞬間
-        if (Input.GetMouseButtonDown(0))
-        {
-            timeText.text = "時間は" + countup.ToString("f2") + "秒です";
-            isClick = true;
-        }
+        if((int)second != (int)oldSeconds) {
+			timeText.text = minute.ToString("00") + ":" + ((int) second).ToString ("f2");
+		}
+		oldSeconds = second;
+    
     }
 }
