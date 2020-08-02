2a8148225ef3bb63e3f4b1c494e864f82607c167
matayu <e185742@ie.u-ryukyu.ac.jp>
2020-08-02 22:00:06 +0900

    show time result to Ending

--- Assets/Scripts/Result.cs
+++ Assets/Scripts/Result.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Result : MonoBehaviour
 {
+
+    public Text timeText;
+    public ResultTime time;
+
     // Start is called before the first frame update
     void Start()
     {
-        float resultScore = PlayerPrefs.GetFloat("SCORE");
-        Debug.Log(resultScore);
+        var json = PlayerPrefs.GetString("SCORE");
+        time = JsonUtility.FromJson<ResultTime>(json);
+        timeText.text = timeText.text = time.minute.ToString("00") + ":" + ((int) time.second).ToString ("00") + ":" + ((int) time.milis).ToString ("000");;
     }
 
     // Update is called once per frame
