6d0246236fbe2ffc45338dfa5064e35fc64f0200
matayu <e185742@ie.u-ryukyu.ac.jp>
2020-08-03 21:02:49 +0900

    add Ending UI

---
+++ Assets/Scripts/EndButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndButton : MonoBehaviour
+{
+    void Quit()
+    {
+      UnityEditor.EditorApplication.isPlaying = false;
+      UnityEngine.Application.Quit();
+    }
+    public void OnClickEndButton()
+    {
+        Quit();
+    }
+}
